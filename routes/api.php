0f283244f6bd55f5e65c30438427895bddcf90de
syahruli28 <syahruli917@gmail.com>
2022-10-30 18:15:38 +0700

    menambahkan todb

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\CekKotaController;
 use App\Http\Controllers\API\CekProvinsiController;
+use App\Http\Controllers\RajaOngkirController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/search/city/{id}', [CekKotaController::class, 'show']);
 // cek Provinsi berdasarkan ID : cth endpoint, http://localhost:8000/api/search/provinces/4
 Route::get('search/provinces/{id}', [CekProvinsiController::class, 'show']);
+
+// jalankan hanya 1 kali, saat tb provinsi dan kota masih kosong
+Route::get('todb', [RajaOngkirController::class, 'tambahKeDb']);
